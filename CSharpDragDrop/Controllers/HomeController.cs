a9e45ef15888d67580bf2de8a33ced40f8ac13ba
Rajdeep Sengupta <rajdeepsengupta31@gmail.com>
2019-04-29 20:08:28 +0530

    Change 2

--- CSharpDragDrop/Controllers/HomeController.cs
+++ CSharpDragDrop/Controllers/HomeController.cs
@@ -41,16 +41,28 @@ namespace CSharpDragDrop.Controllers
         public IActionResult Update(UpdateAC updateAc)
         {
             updateAc.Source.Name = updateAc.Source.Name.Trim();
-            updateAc.Destination.Name = updateAc.Destination.Name.Trim();
+            updateAc.Destination.Name = updateAc.Destination.Name?.Trim();
 
-            if (updateAc.Source.Name.ToLowerInvariant().Contains("country"))
+            if (updateAc.Source.Name.ToLowerInvariant().Contains("country") && updateAc.Destination.Id != -1)
             {
                 return BadRequest("Country can not be moved");
             }
             else if (updateAc.Source.Name.ToLowerInvariant().Contains("state")
-                && updateAc.Destination.Name.ToLowerInvariant().Contains("state"))
+                && (updateAc.Destination.Name == null || updateAc.Destination.Name.ToLowerInvariant().Contains("state")))
             {
                 return BadRequest("State can not be moved into other state");
+            }
+            else if(updateAc.Destination.Id == -1 && updateAc.Source.Name.ToLowerInvariant().Contains("country"))
+            {
+                _list.CountryList.Add(new Country()
+                {
+                    Id = _list.CountryList.Count + 1,
+                    Name = "Country_" + (_list.CountryList.Count + 1)
+                });
+            }
+            else if(updateAc.Destination.Id == -1 && updateAc.Source.Name.ToLowerInvariant().Contains("state"))
+            {
+                return BadRequest("State should be inside a country");
             }
             else
             {
